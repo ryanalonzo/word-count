76895c5609d461d9d016f2a132d9dd26276e8f1e
Ryan Alonzo <yanalonzo25@yahoo.com>
2017-07-30 10:40:27 +0800

    Done, accurate

--- word-count.php
+++ word-count.php
@@ -1,9 +1,9 @@
 <?php
 
 $alice = file_get_contents('alice.txt');
-$pattern = ["'", ',',', ','.','-','`','(',')', ':', '_', '"', '!', ';', '?'];
-$string = str_replace($pattern, '', $alice);
-$needles = explode(' ', strtolower($string));
+$pattern = ["'", '`',',', '"', '-', ':', ';', '.', '!', '?', ')', '(', '*'];
+$string = str_replace($pattern, ' ', $alice);
+$needles = str_word_count(strtolower($string),1);
 foreach ($needles as $key => $val) {
     if(trim($val) == "") {
         unset($needles[$key]);
@@ -11,10 +11,9 @@ foreach ($needles as $key => $val) {
 }
 $vals = array_count_values($needles);
 arsort($vals);
-
-function wordCount($vals, $limit = -1)
+function wordCount($array, $limit = -1)
 {
-    $sliced = array_slice($vals, 0, $limit);
+    $sliced = array_slice($array, 0, $limit);
     foreach($sliced as $key=>$value) {
         echo $key . ' ' . $value . "\n";
     }
